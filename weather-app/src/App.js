8f58b0ad5cc8a8f7aa95493b2c8180f69d21536f
mathews1193 <mathews1193@yahoo.com>
2020-10-21 21:49:31 -0500

    Added city, date, temperature, and weather text

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,6 +6,19 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) =>{
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="app">
       <main>
@@ -15,6 +28,19 @@ function App() {
           className="search-bar"
           placeholder="Search...."
           />
+        </div>
+        <div className="location-box">
+          <div className="location"> New York City, US
+  <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp"> 
+            76°F
+            </div>
+            <div className="weather">
+              Sunny
+            </div>
+          </div>
         </div>
       </main>
     </div>
